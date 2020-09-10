a6a99d313bc8be5ec4c353fc2c660b9178da0416
angel <angelivan.reyes@sanmina.com>
2020-09-10 13:12:37 -0500

    Modificacion de stadistics

--- src/app/components/stadistics/stadistics.component.ts
+++ src/app/components/stadistics/stadistics.component.ts
@@ -4,7 +4,7 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 import { AuthService } from 'src/app/services/auth.service';
 // import { ExportToCsv } from 'export-to-csv';
 //  import * as CSV from 'csv-string';
-import  { ExportToCSV } from "@molteni/export-csv";
+import { ExportToCSV } from "@molteni/export-csv";
 import Swal from 'sweetalert2'
 
 declare var $: any;
@@ -34,7 +34,10 @@ export class StadisticsComponent implements OnInit {
   october: any;
   november: any;
   december: any;
-  ob:any;
+  ob: any;
+  likes: any;
+  allLikes: any;
+  dislikes: any;
   view: any[] = [550, 450];
   showXAxis = true;
   showYAxis = true;
@@ -66,7 +69,7 @@ export class StadisticsComponent implements OnInit {
   ];
 
   ngOnInit() {
-   this.ob=""
+    this.ob = ""
     this.january = 0;
     this.february = 0;
     this.march = 0;
@@ -119,7 +122,7 @@ export class StadisticsComponent implements OnInit {
       // news
 
       // console.log(this.publications);
-        // setTimeout(this.getCharts, 80000);
+      // setTimeout(this.getCharts, 80000);
 
 
     })
@@ -135,10 +138,10 @@ export class StadisticsComponent implements OnInit {
 
 
   exportCSV(data) {
-  //  const arr = this.CSV.parse(data);
-  //  console.log(data);
-   var exporter = new ExportToCSV();
-exporter.exportColumnsToCSV(this.publications, "Conteo De Interacciones",["allLikes"]);
+    //  const arr = this.CSV.parse(data);
+    //  console.log(data);
+    var exporter = new ExportToCSV();
+    exporter.exportColumnsToCSV(this.publications, "Conteo De Interacciones", ["allLikes"]);
     // const options = {
     //   filename: 'Conteo De Interacciones',
     //   fieldSeparator: ',',
@@ -156,80 +159,80 @@ exporter.exportColumnsToCSV(this.publications, "Conteo De Interacciones",["allLi
   }
 
   getCharts() {
-    setTimeout(function(){
-    this.publications.forEach(element => {
-      var likes = element.allLikes;
-      var arrayLikes = new Array();
+    setTimeout(function () {
+      this.publications.forEach(element => {
+        var likes = element.allLikes;
+        var arrayLikes = new Array();
         var arrayDisLikes = new Array();
-      likes.forEach(fechas => {
-        
-        if (fechas.like = false) {
-          var op = fechas.date.slice(5, -13);
-          switch (op) {
-            case '01': arrayDisLikes.push('january'); break;
-            case '02': arrayDisLikes.push('february'); break;
-            case '03': arrayDisLikes.push('march'); break;
-            case '04': arrayDisLikes.push('april'); break;
-            case '05': arrayDisLikes.push('may'); break;
-            case '06': arrayDisLikes.push('june'); break;
-            case '07': arrayDisLikes.push('july'); break;
-            case '08': arrayDisLikes.push('august'); break;
-            case '09': arrayDisLikes.push('september'); break;
-            case '10': arrayDisLikes.push('october'); break;
-            case '11': arrayDisLikes.push('november'); break;
-            case '12': arrayDisLikes.push('december'); break;
+        likes.forEach(fechas => {
 
+          if (fechas.like = false) {
+            var op = fechas.date.slice(5, -13);
+            switch (op) {
+              case '01': arrayDisLikes.push('january'); break;
+              case '02': arrayDisLikes.push('february'); break;
+              case '03': arrayDisLikes.push('march'); break;
+              case '04': arrayDisLikes.push('april'); break;
+              case '05': arrayDisLikes.push('may'); break;
+              case '06': arrayDisLikes.push('june'); break;
+              case '07': arrayDisLikes.push('july'); break;
+              case '08': arrayDisLikes.push('august'); break;
+              case '09': arrayDisLikes.push('september'); break;
+              case '10': arrayDisLikes.push('october'); break;
+              case '11': arrayDisLikes.push('november'); break;
+              case '12': arrayDisLikes.push('december'); break;
+
+            }
+
+          } else if (fechas.like = true) {
+            var op = fechas.date.slice(5, -13);
+            switch (op) {
+              case '01': arrayLikes.push('january'); break;
+              case '02': arrayLikes.push('february'); break;
+              case '03': arrayLikes.push('march'); break;
+              case '04': arrayLikes.push('april'); break;
+              case '05': arrayLikes.push('may'); break;
+              case '06': arrayLikes.push('june'); break;
+              case '07': arrayLikes.push('july'); break;
+              case '08': arrayLikes.push('august'); break;
+              case '09': arrayLikes.push('september'); break;
+              case '10': arrayLikes.push('october'); break;
+              case '11': arrayLikes.push('november'); break;
+              case '12': arrayLikes.push('december'); break;
+
+            }
           }
+        });
+        // aqui se supone acabo la publicacion
+        //       var repetidos = {};
 
-        } else if (fechas.like = true) {
-          var op = fechas.date.slice(5, -13);
-          switch (op) {
-            case '01': arrayLikes.push('january'); break;
-            case '02': arrayLikes.push('february'); break;
-            case '03': arrayLikes.push('march'); break;
-            case '04': arrayLikes.push('april'); break;
-            case '05': arrayLikes.push('may'); break;
-            case '06': arrayLikes.push('june'); break;
-            case '07': arrayLikes.push('july'); break;
-            case '08': arrayLikes.push('august'); break;
-            case '09': arrayLikes.push('september'); break;
-            case '10': arrayLikes.push('october'); break;
-            case '11': arrayLikes.push('november'); break;
-            case '12': arrayLikes.push('december'); break;
+        // arrayLikes.forEach(function(numero){
+        //   repetidos[numero] = (repetidos[numero] || 0) + 1;
+        // });
+        // console.log(repetidos)
+        // var repetidos1 = {};
 
-          }
-        }
+        // arrayDisLikes.forEach(function(numero){
+        //   repetidos1[numero] = (repetidos1[numero] || 0) + 1;
+        // });
+        // console.log(repetidos1)
       });
-      // aqui se supone acabo la publicacion
-//       var repetidos = {};
-
-// arrayLikes.forEach(function(numero){
-//   repetidos[numero] = (repetidos[numero] || 0) + 1;
-// });
-// console.log(repetidos)
-// var repetidos1 = {};
-
-// arrayDisLikes.forEach(function(numero){
-//   repetidos1[numero] = (repetidos1[numero] || 0) + 1;
-// });
-// console.log(repetidos1)
-    });
-    console.log(
-      this.january,
-      this.february,
-      this.march,
-      this.april,
-      this.may,
-      this.june,
-      this.july,
-      this.august,
-      this.september,
-      this.october,
-      this.november,
-      this.december)
-    // switch()
-  },500);
-}
+      console.log(
+        this.january,
+        this.february,
+        this.march,
+        this.april,
+        this.may,
+        this.june,
+        this.july,
+        this.august,
+        this.september,
+        this.october,
+        this.november,
+        this.december)
+      // switch()
+    }, 500);
+  }
 
 
 
@@ -259,10 +262,10 @@ exporter.exportColumnsToCSV(this.publications, "Conteo De Interacciones",["allLi
     }
 
   }
-  delete(key,name){
+  delete(key, name) {
     Swal.fire({
       title: 'Seguro quieres eliminar la publicacin con el titulo',
-      text: name+"",
+      text: name + "",
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -282,30 +285,40 @@ exporter.exportColumnsToCSV(this.publications, "Conteo De Interacciones",["allLi
     // this.firebaseService.deleteNew(key);
     // console.log("se supone que elimino");
   }
-  update(data){
-    this.ob="";
-     console.log("se supone que actualizo",data);
- 
-this.ob=data;
-$("#title").val(this.ob.title);
-$("#video").val(this.ob.video);
-$("#urlimg").val("https://drive.google.com/file/d/"+this.ob.urlimg+"/view?usp=sharing");
-$("#encuesta").val(this.ob.encuesta);
-$("#description").val(this.ob.description);
-$("#endDate").val(this.ob.endDate);
-  }
-  updateAll(data){
-    this.ob.title=$("#title").val(),
-    this.ob.video=$("#video").val(),
-    this.ob.urlimg=$("#urlimg").val().slice(32, -17),
-    this.ob.encuesta=$('select[name=encuesta]').val(),
-    this.ob.description=$("#description").val(),
-    this.ob.endDate=$("#endDate").val()
-      this.firebaseService.updateNew(this.ob.key,this.ob);
-    
+  update(data) {
+    this.ob = "";
+    //  console.log("se supone que actualizo",data);
 
-    setTimeout($('#cerrar').click(),5000);
-    console.log("se supone que actualizocon datos",this.ob);
+    this.ob = data;
+    $("#title").val(this.ob.title);
+    $("#video").val(this.ob.video);
+    if (this.ob.urlimg == "") {
+      $("#urlimg").val("");
+    } else {
+      $("#urlimg").val("https://drive.google.com/file/d/" + this.ob.urlimg + "/view?usp=sharing");
+    }
+    $("#encuesta").val(this.ob.encuesta);
+    $("#description").val(this.ob.description);
+    $("#endDate").val(this.ob.endDate);
+  }
+  updateAll(data) {
+    this.ob.title = $("#title").val(),
+      this.ob.video = $("#video").val(),
+      this.ob.urlimg = $("#urlimg").val().slice(32, -17),
+      this.ob.encuesta = $('select[name=encuesta]').val(),
+      this.ob.description = $("#description").val(),
+      this.ob.endDate = $("#endDate").val()
+    this.firebaseService.updateNew(this.ob.key, this.ob);
+
+
+    setTimeout($('#cerrar').click(), 5000);
+    // console.log("se supone que actualizocon datos",this.ob);
+
+  }
+  getReacciones(likes, dislikes, allLikes) {
+    this.likes = likes;
+    this.dislikes = dislikes;
+    this.allLikes = allLikes;
 
   }
 }
