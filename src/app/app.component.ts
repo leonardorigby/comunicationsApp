81d72275f71c514500f6427d8041e661ae4e21ed
unknown <newsapp.innovation@gmail.com>
2020-10-16 08:38:48 -0500

    se actualiza token en tabla de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AppComponent implements OnInit{
 
   title = 'Comunications App';
+  public currentUser;
   public changetheme: boolean = true;
   // url: string;
 
@@ -32,14 +33,37 @@ export class AppComponent implements OnInit{
 
   public pedirPermisoNotificaciones(){
 
-    
+
     this.afMessaging.getToken.toPromise()
-    
+
     .then( token =>{
 
       if( token ){
-        
+
         console.log('token fresco', token);
+
+        this.auth.getUserData().subscribe(val =>{
+          this.currentUser = val;
+          console.log(this.currentUser);
+          var newuser = {
+            id: this.currentUser.id,
+            fullName: this.currentUser.fullName,
+            email: this.currentUser.email,
+            birthDate: this.currentUser.birthDate,
+            employeeNumber: this.currentUser.employeeNumber,
+            idDepartment: this.currentUser.idDepartment,
+            idPlant: this.currentUser.idPlant,
+            idRole: this.currentUser.idRole,
+            image: this.currentUser.image,
+            authorized: this.currentUser.authorized,
+            AcceptTerminosCondiciones: true,
+            token: token
+          };
+          console.log(newuser);
+          this.auth.registerNewUser(newuser).then(result => {
+            console.log("se actualizo token de usuario");
+          });
+        });
 
         const headers = new HttpHeaders({
           'Authorization': 'key=AAAAkYtejHk:APA91bEjqyrVYjgLiJ-omeOtXNwslB6OQRwDfIbvZh7ESDBSAax_RwkOHaCj-JyA1ai6LROhuSi085GUhnP9xMRyLq4Dw5rJeQRbQpu6vgkIdoKCyQoGrKRDCy-U4bfU5VbUvOCiCFOF'
@@ -51,6 +75,7 @@ export class AppComponent implements OnInit{
         .then( () =>{
 
           console.log('Se guardo el token para el topic all');
+
         })
         .catch(err => {
           console.log('Error al guardar token para el topic all',err);
@@ -67,14 +92,15 @@ export class AppComponent implements OnInit{
     .then( () =>{
       console.log('Se pidio el permiso');
     })
-    
+
     .catch( err => console.log('Error al usar las notificaciones', err) );
 
   }
 
   ngOnInit(){
-
-
+    this.auth.getUserData().subscribe(userSession=>{
+      console.log(userSession)
+    });
     //Para que la notificacion se muestre aunque este abierta la app
     this.fcm.messages.subscribe( (mensaje:any) =>{
 
@@ -82,45 +108,65 @@ export class AppComponent implements OnInit{
 
       new Notification(
          mensaje.notification.title,
-          { body :  mensaje.notification.body } 
+          { body :  mensaje.notification.body }
          );
 
-     
+
     }, err => console.log(err) );
 
 
     if( ! localStorage.getItem('token') ){
 
       this.afMessaging.getToken.toPromise()
-    
+
       .then( token =>{
-  
+
         if( token ){
 
           localStorage.setItem('token', token);
-          
+          this.auth.getUserData().subscribe(val =>{
+            this.currentUser = val;
+            console.log(this.currentUser);
+            var newuser = {
+              id: this.currentUser.id,
+              fullName: this.currentUser.fullName,
+              email: this.currentUser.email,
+              birthDate: this.currentUser.birthDate,
+              employeeNumber: this.currentUser.employeeNumber,
+              idDepartment: this.currentUser.idDepartment,
+              idPlant: this.currentUser.idPlant,
+              idRole: this.currentUser.idRole,
+              image: this.currentUser.image,
+              authorized: this.currentUser.authorized,
+              AcceptTerminosCondiciones: true,
+              token: token
+            };
+            console.log(newuser);
+            this.auth.registerNewUser(newuser).then(result => {
+              console.log("se actualizo token de usuario");
+            });
+          });
           console.log('Token del usuario ', token );
-  
+
         }else{
-  
+
           return this.afMessaging.requestPermission.toPromise();
-  
+
         }
-  
-  
+
+
       } )
       .then( () =>{
         console.log('Se pidio el permiso');
       })
-      
+
       .catch( err => console.log('Error al usar las notificaciones', err) );
 
     }else{
-
       console.log('Ya hay un token guardado', localStorage.getItem('token'));
     }
 
-    
+
 
 
 
